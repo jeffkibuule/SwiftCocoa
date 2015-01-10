e283a422f46ad4d1b4a26ec46055f12db46084ed
Joefrey Kibuule <jeff.kibuule@outlook.com>
2015-01-09 22:30:15 -0600

    Initial Xcode project

---
+++ chapters/HelloCocoa/HelloCocoa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloCocoa
+//
+//  Created by Joefrey Kibuule on 1/9/15.
+//  Copyright (c) 2015 Joefrey Kibuule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
