1e4a7f1ea4e1231ef416ae96baf9f72c4c5ea23c
Joefrey Kibuule <jeff.kibuule@outlook.com>
2015-01-10 14:56:03 -0600

    Complete Constraints Mac app

---
+++ chapters/ch4/Constraints/Constraints/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Constraints
+//
+//  Created by Joefrey Kibuule on 1/10/15.
+//  Copyright (c) 2015 Joefrey Kibuule. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ chapters/ch4/Constraints/Constraints/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Joefrey Kibuule on 1/10/15.
+//  Copyright (c) 2015 Joefrey Kibuule. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
